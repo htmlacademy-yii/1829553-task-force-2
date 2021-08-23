ee0a6992a7e77885fb89efaefdccd43445c18a1a
Alex <mar4ehk0@gmail.com>
2021-08-23 13:09:02 +0300

    fixed test.php

--- test.php
+++ test.php
@@ -9,52 +9,106 @@ use Mar4hk0\Models\Task;
 // - assert.exception = 1
 
 $idCustomer = 10;
+$idSpecialist = 12;
 $testTask = new Task($idCustomer);
+$testTask->setId(111);
+
 $status = 'Новое';
+$i = 1;
 assert(
     $testTask->getStatuses()[Task::STATUS_NEW] === $status,
-    'Test1: failed function getStatuses'
+    "Test$i: failed function getStatuses"
 );
 
 $action = 'Откликнуться';
+$i++;
 assert(
     $testTask->getActions()[Task::ACTION_RESPOND] === $action,
-    'Test2: failed function getActions'
+    "Test$i: failed function getActions"
 );
 
+$i++;
 assert(
     $testTask->getNextStatus(Task::ACTION_START) === Task::STATUS_IN_PROGRESS,
-    'Test3: failed function getNextStatus'
+    "Test$i: failed function getNextStatus"
 );
 
-assert(
-    $testTask->getActionsByStatus(Task::STATUS_NEW, $idCustomer) === [Task::ACTION_CANCEL, Task::ACTION_START],
-    'Test4: failed function getActionsByStatus'
-);
-
+$i++;
 assert(
     $testTask->setStatus(Task::STATUS_NEW) === true,
-    'Test5: failed function setStatus'
+    "Test$i: failed function setStatus"
 );
 
+$i++;
 assert(
     $testTask->setStatus('fake_status') === false,
-    'Test6: failed function setStatus'
+    "Test$i: failed function setStatus"
 );
 
+$i++;
 $statusHuman = 'Отменено';
 $testTask->setStatus(Task::STATUS_CANCELED);
 assert(
     $testTask->getStatusHuman() === $statusHuman,
-    'Test7: failed function getStatusHuman'
+    "Test$i: failed function getStatusHuman"
 );
-
 
 $testTask->setDeadline(time());
 $currentDate = date('Y-m-d');
+$i++;
 assert(
     $testTask->getDeadline() === $currentDate,
-    'Test8: failed function getDeadline'
+    "Test$i: failed function getDeadline"
 );
 
+$testTask->setStatus(Task::STATUS_NEW);
+$actions = $testTask->getActionsByStatus(Task::STATUS_NEW, $idSpecialist);
+$i++;
+assert(
+    $actions[0] instanceof \Mar4hk0\Models\RespondAction,
+    "Test$i: failed, got wrong action"
+);
+
+$actions = $testTask->getActionsByStatus(Task::STATUS_NEW, $idCustomer);
+$i++;
+assert(
+    $actions[0] instanceof \Mar4hk0\Models\CancelAction,
+    "Test$i: failed, got wrong action"
+);
+assert(
+    $actions[1] instanceof \Mar4hk0\Models\StartAction,
+    "Test$i: failed, got wrong action"
+);
+
+$testTask->setIdSpecialist(null);
+$actions = $testTask->getActionsByStatus(Task::STATUS_IN_PROGRESS, $idCustomer);
+$i++;
+assert(
+    empty($actions),
+    "Test$i: failed, got action"
+);
+
+$testTask->setIdSpecialist($idSpecialist);
+$actions = $testTask->getActionsByStatus(Task::STATUS_IN_PROGRESS, $idCustomer);
+$i++;
+assert(
+    $actions[0] instanceof \Mar4hk0\Models\FinishAction,
+    "Test$i: failed, got wrong action"
+);
+
+$testTask->setIdSpecialist($idSpecialist);
+$actions = $testTask->getActionsByStatus(Task::STATUS_IN_PROGRESS, $idSpecialist);
+$i++;
+assert(
+    $actions[0] instanceof \Mar4hk0\Models\RefuseAction,
+    "Test$i: failed, got wrong action"
+);
+
+$testTask->setIdSpecialist($idSpecialist);
+$actions = $testTask->getActionsByStatus(Task::STATUS_IN_PROGRESS, $idSpecialist);
+$i++;
+assert(
+    !($actions[0] instanceof \Mar4hk0\Models\StartAction),
+    "Test$i: failed, got wrong action"
+);
 echo 'All tests passed' . PHP_EOL;
