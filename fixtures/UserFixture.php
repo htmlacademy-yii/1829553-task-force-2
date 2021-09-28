85d1aa6ab8c06a406e53ed4cf57f58480abd0dd8
Alex <mar4ehk0@gmail.com>
2021-09-28 18:51:11 +0300

    исправил шаблон user для поля id_role

--- fixtures/UserFixture.php
+++ fixtures/UserFixture.php
@@ -9,6 +9,17 @@ class UserFixture extends ActiveFixture
 {
     public $modelClass = 'app\models\User';
 
+    public function getAllIdSpecialist(): array
+    {
+        $roleFixture = new RoleFixture();
+        $specialistRoleID = $roleFixture
+            ->getRoleIDByName($roleFixture->getRoleNameSpecialist());
+        return User::find()
+            ->select('id')
+            ->where(['id_role' => $specialistRoleID])
+            ->column();
+    }
+
     public function getRandomIdSpecialist(): int
     {
         $roleFixture = new RoleFixture();
--- fixtures/templates/user.php
+++ fixtures/templates/user.php
@@ -5,6 +5,7 @@
  * @var $index integer
  */
 
+use app\fixtures\RoleFixture;
 use app\models\City;
 use app\models\Role;
 
@@ -16,15 +17,23 @@ $birthday = $faker->dateTimeBetween('-50 years', '-20 years')
 $created = $faker->dateTimeBetween('-6 months', '-1 week')
     ->format('Y-m-d H:i:s');
 
+$roleFixture = new RoleFixture();
+if ($faker->boolean(70)) {
+    $roleName = $roleFixture->getRoleNameSpecialist();
+} else {
+    $roleName = $roleFixture->getRoleNameCustomer();
+}
+$roleId = $roleFixture->getRoleIDByName($roleName);
+
 return [
     'name' => $faker->name,
     'email' => $faker->email,
     'birthday' => $birthday,
     'password' => Yii::$app->getSecurity()->generatePasswordHash('password_' . $index),
     'about' => $faker->about(),
-    'hide_profile' =>  rand(0, 1) == 1,
-    'hide_contacts' =>  rand(0, 1) == 1,
-    'id_role' =>  $faker->randomElement($roleIds),
+    'hide_profile' => $faker->boolean(),
+    'hide_contacts' =>  $faker->boolean(),
+    'id_role' =>  $roleId,
     'id_city' =>  $faker->randomElement($cityIds),
     'rating' => number_format($faker->randomFloat(5, 0, 5), 2),
     'phone' => substr($faker->e164PhoneNumber, 1, 11),
