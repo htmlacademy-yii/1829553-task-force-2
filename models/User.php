bd2030ab7bf255d53cce75721d941f5a026cc903
mar4ehk0 <mar4ehk0@gmail.com>
2021-11-03 15:22:00 +0300

    added field ratting to model User

--- models/User.php
+++ models/User.php
@@ -13,13 +13,14 @@ use Yii;
  * @property string $password
  * @property string $birthday
  * @property int $is_client
- * @property string $about
- * @property string $phone
- * @property string $telegram
+ * @property string|null $about
+ * @property string|null $phone
+ * @property string|null $telegram
  * @property string|null $avatar
- * @property int $hide_contacts
+ * @property int|null $hide_contacts
  * @property int $city_id
  * @property string $created
+ * @property float|null $rating
  *
  * @property City $city
  * @property File[] $files
@@ -43,20 +44,11 @@ class User extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['email',
-                'name',
-                'password',
-                'birthday',
-                'is_client',
-                'about',
-                'phone',
-                'telegram',
-                'hide_contacts',
-                'city_id',
-                'created'], 'required'],
+            [['email', 'name', 'password', 'birthday', 'is_client', 'city_id', 'created'], 'required'],
             [['birthday', 'created'], 'safe'],
             [['is_client', 'hide_contacts', 'city_id'], 'integer'],
             [['about'], 'string'],
+            [['rating'], 'number'],
             [['email', 'name', 'avatar'], 'string', 'max' => 255],
             [['password', 'telegram'], 'string', 'max' => 64],
             [['phone'], 'string', 'max' => 11],
@@ -88,6 +80,7 @@ class User extends \yii\db\ActiveRecord
             'hide_contacts' => 'Скрыть контакты?',
             'city_id' => 'City ID',
             'created' => 'Created',
+            'rating' => 'Rating',
         ];
     }
 
